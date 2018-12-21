0f3b848c42fdf3905c9b34d916e32ff9e39349e0
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2018-12-21 08:45:31 -0800

    made sql file prettier, added background-repeat: no-repeat on body

--- create_data.sql
+++ create_data.sql
@@ -6,6 +6,23 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 SET @dttm = NOW();
 INSERT INTO dojoinfocenter.locations (LocationName, CreatedAt, UpdatedAt) 
-		VALUES ('Mean', @dttm, @dttm), ('BreakoutRoom', @dttm, @dttm), ('Alumni', @dttm, @dttm), ('Python', @dttm, @dttm), ('NorthWomens', @dttm, @dttm), ('NorthMens', @dttm, @dttm), ('Info', @dttm, @dttm), ('PingPong', @dttm, @dttm), ('Kitchen', @dttm, @dttm), ('Presentation', @dttm, @dttm), ('Couch', @dttm, @dttm), ('TeachersLounge', @dttm, @dttm), ('SouthMens', @dttm, @dttm), ('SouthWomens', @dttm, @dttm), ('WebFund', @dttm, @dttm), ('CSharp', @dttm, @dttm), ('FlexSpace', @dttm, @dttm);
+		VALUES 
+			('Mean', @dttm, @dttm), 
+			('BreakoutRoom', @dttm, @dttm), 
+			('Alumni', @dttm, @dttm), 
+			('Python', @dttm, @dttm), 
+			('NorthWomens', @dttm, @dttm), 
+			('NorthMens', @dttm, @dttm), 
+			('Info', @dttm, @dttm), 
+			('PingPong', @dttm, @dttm), 
+			('Kitchen', @dttm, @dttm), 
+			('Presentation', @dttm, @dttm), 
+			('Couch', @dttm, @dttm), 
+			('TeachersLounge', @dttm, @dttm), 
+			('SouthMens', @dttm, @dttm), 
+			('SouthWomens', @dttm, @dttm), 
+			('WebFund', @dttm, @dttm), 
+			('CSharp', @dttm, @dttm), 
+			('FlexSpace', @dttm, @dttm);
 
 SELECT * FROM dojoinfocenter.locations;
